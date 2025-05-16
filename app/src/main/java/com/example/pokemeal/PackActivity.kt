c56e9a9a27ad5a790078a0cb8f776df5fc5f3da2
masonasakura <masonasakura24@gmail.com>
2025-05-16 08:46:19 -0700

    classes

--- app/src/main/java/com/example/pokemeal/PackActivity.kt
+++ app/src/main/java/com/example/pokemeal/PackActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +16,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
