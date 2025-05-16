31339ed0e800b51288594fae6025568af334526e
lizzie chung <elizabethhopechung@gmail.com>
2025-05-16 09:01:36 -0700

    added more missing files

--- app/src/main/java/com/example/pokemeal/PackListActivity.kt
+++ app/src/main/java/com/example/pokemeal/PackListActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PackActivity : AppCompatActivity() {
+class PackListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pack)
+        setContentView(R.layout.activity_pack_list)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
