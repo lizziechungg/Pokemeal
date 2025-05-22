7fa8d5f5fba75b709a1ec5e98c3c4c7b22a03da9
lizzie chung <elizabethhopechung@gmail.com>
2025-05-22 08:50:42 -0700

    added more missing files

--- app/src/main/java/com/example/pokemeal/PackListActivity.kt
+++ app/src/main/java/com/example/pokemeal/PackListActivity.kt
@@ -1,20 +1,53 @@
 package com.example.pokemeal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PackListActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pack_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.pack_list)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.selectedItemId = R.id.packs
+        bottomNavigationView.selectedItemId = R.id.recipes
+        bottomNavigationView.selectedItemId = R.id.foodLog
+        // Perform item selected listener
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val id = item.itemId
+
+            when (id) {
+                R.id.packs -> {
+                    startActivity(Intent(applicationContext, PackListActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+
+                R.id.recipes -> {
+                    startActivity(Intent(applicationContext, PackCollectionActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+                R.id.foodLog -> {
+                    startActivity(Intent(applicationContext, FoodLogActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+
+                else -> false
+            }
         }
 
 
